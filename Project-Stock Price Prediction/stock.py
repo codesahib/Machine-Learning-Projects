924db85d45c7c54f76a46ccb896509746f6500e8
Gursahib Singh <gursahib.kvm@gmail.com>
2019-04-24 16:06:40 +0530

    Updated comments

--- Project-Stock Price Prediction/stock.py
+++ Project-Stock Price Prediction/stock.py
@@ -9,91 +9,46 @@ from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing, cross_validation, svm
 #get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# In[8]:
-
-
 style.use('ggplot')
 
-
-# In[19]:
-
-
+#Enter start time and end time to get required data
 start=dt.datetime(2016,1,1)
-end=dt.datetime(2018,9,24)
+end=dt.datetime(2019,3,23)
 
-
-# In[52]:
-
-
+#dataframe to get data related to stock entered
 df=web.DataReader('SBIN.NS','yahoo',start,end)
 
-
-
-# In[53]:
-
-
+#Data contains too many fields but we have to use only one field
 df=df[['Adj Close']]
 
-
-
-# In[65]:
-
-
+#Enter the number of days to forecast data for, here 30
 forecast_out = int(30) 
 df['Prediction'] = df[['Adj Close']].shift(-forecast_out)
 
-
-
-# In[85]:
 
 X = np.array(df.drop(['Prediction'], 1))
 X = preprocessing.scale(X)
 X.shape
 
 
-# In[86]:
-
-
 X_forecast = X[-forecast_out:] # set X_forecast equal to last 30
 X = X[:-forecast_out] # remove last 30 from X
 X.shape
-
-
-# In[90]:
-
 
 y = np.array(df['Prediction'])
 y = y[:-forecast_out]
 y.shape
 
-
-# In[91]:
-
-
+#divide the dataset into training and testing data
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
-
-
-# In[92]:
-
 
 # Training
 clf = LinearRegression()
 clf.fit(X_train,y_train)
+
 # Testing
 confidence = clf.score(X_test, y_test)
 print("confidence: ", confidence)
 
-
-# In[82]:
-
-
 forecast_prediction = clf.predict(X_forecast)
 print(forecast_prediction)
-
-
-# In[ ]:
-
-
-
-
